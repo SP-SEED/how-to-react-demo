4691ad12a0d0dde8ef73dffcdbd99bcde82be4ed
Acrylic125 <acrylic125email@gmail.com>
2022-09-28 11:03:09 +0800

    feat: component demo

---
+++ src/starts/Component.start.jsx
@@ -0,0 +1,15 @@
+/**
+ * In this file, we will demonstrate the following:
+ * 1. Creating a simple component.
+ * 2. Using props.
+ * 3. Composition
+ * 4. Rendering Iterated Components
+ */
+import React from "react";
+
+// function Center() {
+// }
+
+function Comp() {}
+
+export default Comp;
